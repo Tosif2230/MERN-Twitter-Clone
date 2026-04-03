d7063ea56e27b33244991935c7abc2aebbf0e643
Mohammed Tosif <techie.tosif@gmail.com>
2026-04-03 16:50:25 +0530

    Extend NextConfig to include ESLint options

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig & {
+  eslint?: {
+    ignoreDuringBuilds?: boolean;
+  };
+} = {
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
   },
